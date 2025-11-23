abbb2ce63402bde5f5480cfe9ee9c00a5f139ceb
Manender <Manendeer@gmail.com>
2025-11-23 14:46:22 +0500

    Work on the frog

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,11 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 	[Header("Rigidbody 2D")]
-	[Tooltip("Rigidbody 2D")]
-	[SerializeField] private Rigidbody2D _rb;
+	[SerializeField] Rigidbody2D _rb;
 
 	[Header("Spinning")]
 	[Tooltip("Rotation speed in degrees per second")]
@@ -17,28 +17,38 @@ public class Player : MonoBehaviour {
 	[Tooltip("Jump time in seconds")]
 	public float jumpTime;
 	public float jumpForce;
-
+	
+	State state = State.Sitting;
+	public enum State {
+		Sitting, Jumping, PreparingForJump
+	}
+	
+	[Header("Death by overhydration")]
+	public LayerMask ground;
+	public Collider2D groundContactCollider;
+	bool IsOnGround {
+		get {
+			List<Collider2D> colliders = new();
+			foreach (Collider2D collider in colliders) {
+				print(collider.name);
+			}
+			return groundContactCollider.GetContacts(new ContactFilter2D() { layerMask = ground, useTriggers = true }, colliders) > 0;
+		}
+	}
+	
 	[Header("Sprites")]
-	[SerializeField] private SpriteRenderer _frog;
+	[SerializeField] SpriteRenderer sprite;
 	public Sprite frogJump;
 	public Sprite frogSit;
 
 	[Header("Slider")]
 	public Slider slider;
-	private float _startSlider = 5;
-
-	public bool isOnGround = true;
-
-	State state = State.Sitting;
-
-	public enum State {
-		Sitting, Jumping, PreparingForJump
-	}
+	float _startSlider = 5;
 
 	private void Start() {
-		_frog.sprite = frogSit;
+		sprite.sprite = frogSit;
 
-		slider.gameObject.SetActive(false);
+		slider?.gameObject.SetActive(false);
 	}
 
 	void FixedUpdate() {
@@ -49,21 +59,20 @@ public class Player : MonoBehaviour {
 		if (state == State.Sitting) SittingUpdate();
 
 		VisualForceJump();
-
-		CheckGround();
 	}
 
 	void SittingUpdate() {
-		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+		if (Input.GetKeyDown(KeyCode.Mouse0) && state == State.Sitting) {
 			StartCoroutine(Jump());
 		}
 	}
 
 	IEnumerator Jump() {
 		state = State.PreparingForJump;
-		
+
 		while (Input.GetKey(KeyCode.Mouse0)) {
-			slider.gameObject.SetActive(true);
+			slider?.gameObject.SetActive(true);
+			
 			jumpForce += Time.deltaTime * 5;
 			jumpTime += Time.deltaTime;
 			_startSlider -= Time.deltaTime * 5;
@@ -78,32 +87,28 @@ public class Player : MonoBehaviour {
 		}
 
 		state = State.Jumping;
-		slider.gameObject.SetActive(false);
+		slider?.gameObject.SetActive(false);
 		_rb.linearVelocity = _rb.GetRelativeVector(Vector2.up) * jumpDistance / jumpTime * (jumpForce + 1);
-		_frog.sprite = frogJump;
+		sprite.sprite = frogJump;
 
 		yield return new WaitForSeconds(jumpTime);
 
 		_rb.linearVelocity = Vector2.zero;
-		_frog.sprite = frogSit;
+		sprite.sprite = frogSit;
 		jumpTime = 0.2f;
 		jumpForce = 0;
 		_startSlider = 5;
 		state = State.Sitting;
+		
 		DieIfNotOnGround();
 	}
 
 	void DieIfNotOnGround() {
-		if (isOnGround == false) print("you are supposed to die");
+		if (!IsOnGround) print("you are supposed to die");
 	}
 
 	void VisualForceJump() {
-		slider.value = _startSlider;
+		if (slider is not null) slider.value = _startSlider;
 	}
-
-	public LayerMask ground;
-
-	void CheckGround () {
-		isOnGround = Physics2D.OverlapCircle(transform.position, 0.5f, ground);
-	}
+	
 }
